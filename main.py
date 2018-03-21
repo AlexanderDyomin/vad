758380f707450b3c3acbe389b60fce65c4ed2eb5
Alex Talker <alextalker@yandex.ru>
2018-03-21 15:35:41 +0300

    Improve command-line interface

--- main.py
+++ main.py
@@ -1,26 +1,32 @@
-import sys
-
-from sklearn.model_selection import GridSearchCV, ShuffleSplit
+import argparse
 
 from vad import DataSet, Config, Detector
 
-# SQLMap - useful tool
-def foo():
-    config = Config('config.json')
-    detector = Detector(config)
-    detector.load()
-    data_set = DataSet(sys.argv[1], config)
-    search = detector.teach(data_set)
-    print('Best parameters:', search.best_params_)
-    print('Best score:',      search.best_score_)
+def process_args():
+    parser = argparse.ArgumentParser(description='Detector of vulnerabilities')
+    parser.add_argument('config', metavar='config', nargs='?', type=str, default='config.json')
+    parser.add_argument('-d', '--dataset', dest='dataset', type=str)
+    parser.add_argument('-l', '--load', dest='load', type=str, default=None)
+    parser.add_argument('-s', '--save', dest='save', type=str, default=None)
+    return parser.parse_args()
 
 # TODO: Datasets
+# SQLMap - useful tool
 # Tune network
 # true positive/negative, other metrics
 # CLI
 # Error function plot during education
 
 if __name__ == '__main__':
-    foo()
-    #path = sys.argv[1]
-    #teach(path, 'model_dump')
+    args = process_args()
+    cfg = Config(args.config)
+    detector = Detector(cfg)
+    detector.load(args.load)
+    if args.dataset:
+        data_set = DataSet(args.dataset, cfg)
+        search = detector.teach(data_set)
+        print('Best parameters:', search.best_params_)
+        print('Best score:',      search.best_score_)
+    # TODO: Prediction
+    if args.save:
+        detector.save(args.save)
